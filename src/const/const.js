e3ce423b16eca7e8b68c45380b1fd2804b7ec091
gonreynoso <gonzalo.reynoso9@gmail.com>
2024-09-15 18:39:20 -0300

    Add Gmail link to footer and update portfolio projects

--- src/const/const.js
+++ src/const/const.js
@@ -130,9 +130,9 @@ export const skills = [
 
 export const projects = [
   {
-    img: "../../../img/tasks-manager.webp",
-    title: "Tasks Manager",
-    link: "https://task-manager-ecru-chi.vercel.app/dashboard",
+    img: "../../../img/vivero-takumi.webp",
+    title: "Vivero Takumi",
+    link: "https://viverotakumi.com.ar/",
     description: `Administrador de tareas con drag and drop autenticación con Clerk , shadcn.  "En Desarrollo "`,
     technologies: [
       {
@@ -153,10 +153,10 @@ export const projects = [
     ],
   },
   {
-    img: "../../../img/ecommerce-stripe-2.webp",
-    title: "Ecommerce Stripe",
-    link: "https://ecommerce-stripe-763m.vercel.app/",
-    description: `Proyecto FullStack de un ecommerce con pasarela de pagos stripe integrada, destalles de el product etc.  `,
+    img: "../../../img/informental.webp",
+    title: "Informental",
+    link: "https://informental.com.ar/",
+    description: `Portafolio de un ecommerce con pasarela de pagos stripe integrada, destalles de el product etc.  `,
     technologies: [
       {
         icon: IconKeys.typeScript,
@@ -176,10 +176,10 @@ export const projects = [
     ],
   },
   {
-    img: "../../../img/nexanime.webp",
-    title: "NexAnime",
-    link: "https://nexanime.vercel.app",
-    description: `plataforma de anime para ver los animes en tendencia , más valorados , estadísticas.`,
+    img: "../../../img/medicina-diagnostica.webp",
+    title: "Medicina Diagnóstica",
+    link: "https://medicinadiagnostica.com.ar/",
+    description: `Portafolio de una aplicación de medicina diagnostica.`,
     technologies: [
       {
         icon: IconKeys.typeScript,
@@ -192,137 +192,138 @@ export const projects = [
       },
     ],
   },
-  {
-    img: "../../../img/store-games.webp",
-    title: "Store games",
-    link: "https://store-games-site.netlify.app/",
-    description: `Simulando una página de juegos , consumiendo la API de rawg api
-      donde los usuarios pueden buscar videojuegos, ver sus detalles como
-      creadores , calificaciones,tiendas donde comprar.....`,
-    technologies: [
-      {
-        icon: IconKeys.react,
-      },
-      {
-        icon: IconKeys.css,
-      },
-      {
-        icon: IconKeys.javaScript,
-      },
-      {
-        icon: IconKeys.tailwind,
-      },
-      {
-        icon: IconKeys.reactQuery,
-      },
-    ],
-  },
-  {
-    img: "../../../img/movie.webp",
-    title: "Movie Tv",
-    link: "https://movietvsite.netlify.app/",
-    description: `simulando una página de películas , consumiendo la API de themovietv
-              donde los usuarios pueden buscar películas y ver trailer.....`,
-    technologies: [
-      {
-        icon: IconKeys.react,
-      },
-      {
-        icon: IconKeys.css,
-      },
-      {
-        icon: IconKeys.javaScript,
-      },
-      {
-        icon: IconKeys.tailwind,
-      },
-    ],
-  },
-  {
-    img: "../../../img/cyberpunk.webp",
-    title: "cyberpunk",
-    link: "https://preeminent-liger-266356.netlify.app/",
-    description: `este proyecto se trata de un clon de la pagína de cyberpunk , donde
-      el ususario puede ver los tailer de él juegos y también los
-      episodios de la serie....`,
-    technologies: [
-      {
-        icon: IconKeys.html,
-      },
-      {
-        icon: IconKeys.css,
-      },
-      {
-        icon: IconKeys.javaScript,
-      },
-    ],
-  },
+  // },
+  // {
+  //   img: "../../../img/store-games.webp",
+  //   title: "Store games",
+  //   link: "https://store-games-site.netlify.app/",
+  //   description: `Simulando una página de juegos , consumiendo la API de rawg api
+  //     donde los usuarios pueden buscar videojuegos, ver sus detalles como
+  //     creadores , calificaciones,tiendas donde comprar.....`,
+  //   technologies: [
+  //     {
+  //       icon: IconKeys.react,
+  //     },
+  //     {
+  //       icon: IconKeys.css,
+  //     },
+  //     {
+  //       icon: IconKeys.javaScript,
+  //     },
+  //     {
+  //       icon: IconKeys.tailwind,
+  //     },
+  //     {
+  //       icon: IconKeys.reactQuery,
+  //     },
+  //   ],
+  // },
+  // {
+  //   img: "../../../img/movie.webp",
+  //   title: "Movie Tv",
+  //   link: "https://movietvsite.netlify.app/",
+  //   description: `simulando una página de películas , consumiendo la API de themovietv
+  //             donde los usuarios pueden buscar películas y ver trailer.....`,
+  //   technologies: [
+  //     {
+  //       icon: IconKeys.react,
+  //     },
+  //     {
+  //       icon: IconKeys.css,
+  //     },
+  //     {
+  //       icon: IconKeys.javaScript,
+  //     },
+  //     {
+  //       icon: IconKeys.tailwind,
+  //     },
+  //   ],
+  // },
+  // {
+  //   img: "../../../img/cyberpunk.webp",
+  //   title: "cyberpunk",
+  //   link: "https://preeminent-liger-266356.netlify.app/",
+  //   description: `este proyecto se trata de un clon de la pagína de cyberpunk , donde
+  //     el ususario puede ver los tailer de él juegos y también los
+  //     episodios de la serie....`,
+  //   technologies: [
+  //     {
+  //       icon: IconKeys.html,
+  //     },
+  //     {
+  //       icon: IconKeys.css,
+  //     },
+  //     {
+  //       icon: IconKeys.javaScript,
+  //     },
+  //   ],
+  // },
 
-  {
-    img: "../../../img/fit-nation.webp",
-    title: "gym",
-    link: "https://fitnation.netlify.app/",
-    description: `landing page de una gym por ahora es una landing todavia está en
-              desarrollo la idea es que los usuarios puedan comprar productos de
-              el gym`,
-    technologies: [
-      {
-        icon: IconKeys.react,
-      },
-      {
-        icon: IconKeys.css,
-      },
-      {
-        icon: IconKeys.javaScript,
-      },
-      {
-        icon: IconKeys.tailwind,
-      },
-    ],
-  },
-  {
-    img: "../../../img/spotify.webp",
-    title: "Spotify-Clone",
-    link: "https://www.linkedin.com/posts/osnaider-mart%C3%ADnez_clon-de-la-p%C3%A1gina-de-spotify-echo-con-react-activity-7119712602342965248-upgL?utm_source=share&utm_medium=member_desktop",
-    description: `Clon de la página de Spotify con autenticación de usuario, reproductor de canciones, etc `,
-    technologies: [
-      {
-        icon: IconKeys.react,
-      },
-      {
-        icon: IconKeys.javaScript,
-      },
-      {
-        icon: IconKeys.tailwind,
-      },
-      {
-        icon: IconKeys.redux,
-      },
-      {
-        icon: IconKeys.reactQuery,
-      },
-    ],
-  },
+  // {
+  //   img: "../../../img/fit-nation.webp",
+  //   title: "gym",
+  //   link: "https://fitnation.netlify.app/",
+  //   description: `landing page de una gym por ahora es una landing todavia está en
+  //             desarrollo la idea es que los usuarios puedan comprar productos de
+  //             el gym`,
+  //   technologies: [
+  //     {
+  //       icon: IconKeys.react,
+  //     },
+  //     {
+  //       icon: IconKeys.css,
+  //     },
+  //     {
+  //       icon: IconKeys.javaScript,
+  //     },
+  //     {
+  //       icon: IconKeys.tailwind,
+  //     },
+  //   ],
+  // },
+  // {
+  //   img: "../../../img/spotify.webp",
+  //   title: "Spotify-Clone",
+  //   link: "https://www.linkedin.com/posts/osnaider-mart%C3%ADnez_clon-de-la-p%C3%A1gina-de-spotify-echo-con-react-activity-7119712602342965248-upgL?utm_source=share&utm_medium=member_desktop",
+  //   description: `Clon de la página de Spotify con autenticación de usuario, reproductor de canciones, etc `,
+  //   technologies: [
+  //     {
+  //       icon: IconKeys.react,
+  //     },
+  //     {
+  //       icon: IconKeys.javaScript,
+  //     },
+  //     {
+  //       icon: IconKeys.tailwind,
+  //     },
+  //     {
+  //       icon: IconKeys.redux,
+  //     },
+  //     {
+  //       icon: IconKeys.reactQuery,
+  //     },
+  //   ],
+  // },
 
-  {
-    img: "../../../img/landing.webp",
-    title: "Landing Realidad Virtual",
-    link: "https://realityvirtual.netlify.app/",
-    description: `landing page de gafas de realidad virtual donde se muestra el
-              producto y como funciona`,
-    technologies: [
-      {
-        icon: IconKeys.astro,
-      },
-      {
-        icon: IconKeys.css,
-      },
-      {
-        icon: IconKeys.javaScript,
-      },
-      {
-        icon: IconKeys.tailwind,
-      },
-    ],
-  },
+  // {
+  //   img: "../../../img/landing.webp",
+  //   title: "Landing Realidad Virtual",
+  //   link: "https://realityvirtual.netlify.app/",
+  //   description: `landing page de gafas de realidad virtual donde se muestra el
+  //             producto y como funciona`,
+  //   technologies: [
+  //     {
+  //       icon: IconKeys.astro,
+  //     },
+  //     {
+  //       icon: IconKeys.css,
+  //     },
+  //     {
+  //       icon: IconKeys.javaScript,
+  //     },
+  //     {
+  //       icon: IconKeys.tailwind,
+  //     },
+  //   ],
+  // },
 ];
