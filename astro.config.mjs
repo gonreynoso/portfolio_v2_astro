36053b0d343d7ab4b994c456354e576b12125a46
gonreynoso <gonzalo.reynoso9@gmail.com>
2024-09-20 12:48:30 -0300

    adding tailwind CSS y plugin typography

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,17 @@ import vercel from "@astrojs/vercel/serverless";
 
 // If you are not going to use vercel analytics you can delete the analytics
 // If you want to remove vercel from your project you can do npm uninstall vercel
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
-    },
+      enabled: true
+    }
   }),
+  integrations: [tailwind()]
 });
 
 //If you don't have vercel the configuration will be empty
